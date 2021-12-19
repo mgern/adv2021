aeee68fd19b1c7745df0f2e791ecccad0bb91634
mgern <maximiliang60@gmail.com>
2021-12-19 19:25:03 +1300

    adv1-2

--- adv1-2.py
+++ adv1-2.py
@@ -3,10 +3,24 @@
 f = open("1input", "r")
 prev = 0
 count = 0
+lines = []
+
+a1 = a2 = a3 = 0
 for line in f:
+    lines.append(int(line))
+
+# print(len(lines) - 2)
+for i in range(len(lines) - 2):
+    a1 = lines[i]
+    a2 = lines[i+1]
+    a3 = lines[i+2]
     if prev != 0:
-        if int(line) > prev:
+        #is this higher than previous?
+        if (a1 + a2 + a3) > prev:
             count += 1
-    prev = int(line)
+
+    prev = a1 + a2 + a3
+
+
 
 print(count)
