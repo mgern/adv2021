d3d4fc57a11b6e70908fd25edd1306dbeb04f011
mgern <maximiliang60@gmail.com>
2021-12-20 21:09:49 +1300

    startin 3-2

--- adv3-2.py
+++ adv3-2.py
@@ -1,46 +1,46 @@
 f = open("3input")
 
-# gamma = 0 #gamma = bits of most common bits in each row
-# epsilon = 0 #least common = gamma xor'ed with 11111
-counts = [0,0,0,0,0,0,0,0,0,0,0,0]
-linecount = 0
+#oxygen = 
+#go through the first digit of each numbers and find the most common
+#keep the most common ones
+#repeat for the next digit
+
+
+#convert file into a 2d array
+#make array or length of first line
+
+lines = []
+
 for x in f:
-    # binary = int(x, 2)
-    # print(binary)
-    counts[0] += int(x[0])
-    counts[1] += int(x[1])
-    counts[2] += int(x[2])
-    counts[3] += int(x[3])
-    counts[4] += int(x[4])
-    counts[5] += int(x[5])
-    counts[6] += int(x[6])
-    counts[7] += int(x[7])
-    counts[8] += int(x[8])
-    counts[9] += int(x[9])
-    counts[10] += int(x[10])
-    counts[11] += int(x[11])
-    # counts[12] += int(x[12])
-    
-    linecount += 1
+    lines.append(x)
 
-print(counts)
 
-gamma = []
-for n in counts:
-    if n > linecount / 2:
-        gamma.append("1")
+#for each column
+for x in range(len(lines[0])):
+    zeroes = ones = 0
+    for y in lines:
+        if y[x] == "0":
+            zeroes += 1
+        else:
+            ones += 1
+    if zeroes > ones:
+        for i in lines:
+            if i[x] == "1":
+                lines.remove(x)
     else:
-        gamma.append("0")
+        for i in lines:
+            if i[x] == "0":
+                lines.remove(i)
+    print("\n")
+    print(lines)
+    print(x)
 
-gamma = ''.join(gamma)
-epsilon = gamma.replace("1","4").replace("0","1").replace("4","0")
-
-power = int(gamma,2) * int(epsilon, 2)
-
+    
 
 O2 = C02 = 0
 
 
+
 lifesupport = O2 * C02
 
-print(power)
+print(lifesupport)
